b1b38dc832534dd02d073e39fe693de780a3e00e
Arnav Sabharwal <arnavsab@andrew.cmu.edu>
2024-03-12 03:19:38 -0400

    progress made, with differing set of errors?

--- Qpf/Examples/_02_Tree.lean
+++ Qpf/Examples/_02_Tree.lean
@@ -1,6 +1,7 @@
 import Qpf
 import Qpf.Examples._01_List
 import Qpf.Macro.Tactic.FinDestr
+import Qpf.MathlibPort.Fin2
 -- import Qpf.Qpf.Multivariate.Constructions.Comp
 
 open MvQPF
@@ -38,11 +39,14 @@ namespace QpfTree
     abbrev ChildT : HeadT → TypeVec 2
       -- | .leaf => ![PFin2 1, PFin2 0, PFin2 0, PFin2 0]
       | .node => ![PFin2 1, PFin2 1]
+      --| .node, _ => Unit
 
     abbrev P := MvPFunctor.mk HeadT ChildT
 
     -- abbrev F := P.Obj.curried
     abbrev F := TypeFun.curried P.Obj
+
+    instance : MvFunctor P.Obj := by infer_instance
   end Shape
 
   /-
@@ -69,23 +73,111 @@ namespace QpfTree
     ```
   -/
 
-  #check @Prj 2 1
-  #check Shape.F
+  #check Fin2
+  #check Fix.mk
 
-  def G := !![
-        @Prj 2 1,
-        Comp QpfList' !![@Prj 2 0]
+  abbrev G' (j : Fin2 1) : TypeVec 2 → Type := Prj 0
+
+  abbrev G (i : Fin2 2) : TypeVec 2 → Type :=
+    match i with
+    | 0 => Comp QpfList' G'
+    | 1 => Prj 1
+
+
+  /-
+  def G : Fin2 2 → TypeVec 2 -> Type := !![
+        Prj 1,
+        --Comp QpfList' !![@Prj 2 0]
+        Comp QpfList' ![@Prj 2 0]
     ]
+  -/
+
+  #check ![@Prj 2 0]
+  #check !![@Prj 2 0]
 
   abbrev Base : TypeFun 2
     := Comp Shape.P.Obj G
 
   def F' := Shape.P.Obj
 
+  instance : MvFunctor Shape.P.Obj := by infer_instance
+  instance : MvQPF Shape.P.Obj := by infer_instance
+
+  instance (i : Fin2 2) : MvFunctor (G i) := by
+    match i with
+    | .fz => simp [G]; infer_instance
+    | .fs s =>
+      simp [G]
+      split
+      · contradiction      -- Alternatively, rename _ => h; revert h; exact Fin2.elim0
+      · infer_instance
+
+  instance : MvFunctor Base := by infer_instance
+
+  -- CC: This is having trouble type-checking?
+  --instance : MvQPF (fun (i : Fin2 1) =>
+  --    Matrix.vecCons (Prj 1) ![] (Fin.rev (PFin2.toFin (PFin2.ofFin2 i)))) := by
+
+  --  done\
+
+  #check Comp
+
+  instance (i : Fin2 2) : MvQPF (G i) := by
+    match i with
+    | .fz => simp [G]; infer_instance
+    | .fs s =>
+      simp [G]
+      match s with
+      | .fz =>
+        simp
+        infer_instance
+        done
+      | .fs s' => revert s'; exact Fin2.elim0
+
+  instance : MvQPF Base := by infer_instance
+
+  abbrev QpfTree' := Fix Base
+  abbrev QpfTree  := TypeFun.curried QpfTree'
+
+  #check @Prj 2 1
+  #check Shape.F
+  #check Shape.P.Obj
+  #check Vec.append1
+
+  #check ![@Prj 2 0]
+  #check !![@Prj 2 0]
+
+  instance : MvFunctor QpfList' := by infer_instance
+
+
   /-
     There is also a `qpf` command, which will define the right projections and compositions for us!
   -/
-  -- qpf F_curried α β := Shape.F α (QpfList β)
+  qpf F_curried α β := Shape.F α (QpfList β)
+
+  --abbrev F := F_curried.typefun
+
+    -- -- Unfold the definitions, to see both are applications of `Comp`
+    -- dsimp [F_manual, F_curried.typefun]
+    -- apply congrArg;
+
+    -- -- At this point, the goal is to show that two vectors, of known size are equal.
+    -- -- These vectors are not definitially equal, but their respective elements are.
+    -- funext i;
+    -- -- The following tactic takes a `i : Fin2 n`, where `n` is a known constant, and breaks the goal
+    -- -- into `n` subgoals, one for each concrete possible value of `i`
+    -- fin_destr i <;> rfl
+
+
+
+  /-
+    Type class inference works as expected, it can reason about the vectors of functors involved
+    in compositions
+  -/
+  --example : MvQPF F := by infer_instance
+
+  --abbrev QpfTree' := Fix F
+  --abbrev QpfTree  := QpfTree'.curried
 
   /-
     The command will give us a curried type function, the internal, uncurried, function can be found
@@ -126,19 +218,53 @@ namespace QpfTree
   --   abs_repr := sorry
   --   abs_map := sorry
 
-  instance : MvFunctor Base where map := sorry
+  -- instance : MvFunctor Base where map := sorry
 
   -- example : MvQPF Base := by infer_instance
 
-  instance : MvQPF Base where
-    P := sorry
-    abs := sorry
-    repr {α} := sorry
-    abs_repr := sorry
-    abs_map := sorry
+  /-
+  abbrev Base : TypeFun 2
+    := Comp Shape.P.Obj G
 
-  abbrev QpfTree' := Fix Base
-  abbrev QpfTree  := TypeFun.curried QpfTree'
+      nm: ℕ
+      F: TypeVec.{u} n → Type u_1
+      fF: MvFunctor F
+      q: MvQPF F
+      G: Fin2 n → TypeVec.{u} m → Type u
+      fG: (i : Fin2 n) → MvFunctor (G i)
+      q': (i : Fin2 n) → MvQPF (G i)
+      αβ: TypeVec.{u} m
+      f: α ⟹ β
+      ⊢ MvPFunctor.{u} m
+
+  instance : MvQPF (Comp F G) where
+  P := MvPFunctor.comp (P F) fun i ↦ P <| G i
+  abs := Comp.mk ∘ (map fun i ↦ abs) ∘ abs ∘ MvPFunctor.comp.get
+  repr {α} := MvPFunctor.comp.mk ∘ repr ∘
+              (map fun i ↦ (repr : G i α → (fun i : Fin2 n ↦ Obj (P (G i)) α) i)) ∘ Comp.get
+  abs_repr := by
+    intros
+    simp (config := { unfoldPartialApp := true }) only [Function.comp_def, comp.get_mk, abs_repr,
+      map_map, TypeVec.comp, MvFunctor.id_map', Comp.mk_get]
+  abs_map := by
+    intros
+    simp only [(· ∘ ·)]
+    rw [← abs_map]
+    simp (config := { unfoldPartialApp := true }) only [comp.get_map, map_map, TypeVec.comp,
+      abs_map, map_mk]
+  -/
+
+  --instance : MvQPF (Comp Shape.P.Obj G) := by infer_instance
+
+  -- instance : MvQPF Base where
+  --   P := MvPFunctor.comp (P Shape.P.Obj) fun i ↦ P <| G i
+  --   abs := sorry
+  --   repr {α} := sorry
+  --   abs_repr := sorry
+  --   abs_map := sorry
+
+  --abbrev QpfTree' := Fix Base
+  --abbrev QpfTree  := TypeFun.curried QpfTree'
 
   namespace debug
 
@@ -185,8 +311,73 @@ namespace QpfTree
 
   #check TypeVec.Arrow
 
+  #check TypeFun
+
   #check Comp
   #check Vec.reverse_involution
+
+  #check PFin2
+  #check Vec
+
+  -- abbrev long_type (β : Type) := TypeVec.Arrow (Shape.ChildT Shape.HeadT.node) fun i =>
+  -- G i ((Vec.reverse (Vec.append1 Vec.nil β)) ::: (Fix Base (Vec.reverse (Vec.append1 Vec.nil β))))
+
+  theorem curried_uncurried_equal (β : Type)
+  -- (f : TypeVec.Arrow (Shape.ChildT Shape.HeadT.node) fun i =>
+  -- G i ((Vec.reverse (Vec.append1 Vec.nil β)) ::: (Fix Base (Vec.reverse (Vec.append1 Vec.nil β)))))
+  : QpfList' (fun (i : Fin2 1) => QpfTree β) = QpfList (QpfTree β) := by
+  {
+    suffices ts : (TypeFun.curriedAux (QpfList')) (QpfTree β) = QpfList' (fun (i : Fin2 1) => QpfTree β)
+    suffices ts2 : ∀ v : TypeVec 1, (TypeFun.reverseArgs QpfList') v = QpfList' v
+    let ts3 : TypeFun.reverseArgs QpfList' = QpfList':= funext ts2
+    let ts4 : QpfList' (fun (i : Fin2 1) => QpfTree β) = QpfList (QpfTree β) := by {
+      let rw1 : QpfList = TypeFun.curried (QpfList') := by trivial
+      rw [rw1]
+      rw [TypeFun.curried, ts3, ts]
+    };
+    trivial
+
+    intro v
+    rw [TypeFun.reverseArgs]
+    apply congrArg
+    let test2 : ∀ j : Fin2 1, (Vec.reverse v) j = v j := by {
+      intro j
+      conv =>
+        lhs
+        simp [Vec.reverse, Fin2.inv]
+      apply congrArg
+      cases (PFin2.ofFin2 j)
+      let test3 : @PFin2.inv 1 (PFin2.fz) = PFin2.fz := by trivial
+      rw [test3]
+      rw [← (@PFin2.ofFin2_toFin2_iso 1 j)]
+      apply congrArg
+      cases (PFin2.ofFin2 j)
+      trivial
+      contradiction
+      contradiction
+    };
+    exact funext test2
+
+    conv =>
+        lhs
+        rw [TypeFun.curriedAux]
+        simp
+
+    -- apply congrArg
+    -- let test1 : ∀ i : Fin2 1, (Vec.append1 Vec.nil (QpfTree β)) i = (fun i => QpfTree β) i := by {
+    --   intro i
+    --   conv =>
+    --     rhs
+    --     simp
+    --   conv =>
+    --     lhs
+    --     simp [Vec.append1]
+    --   split
+    --   contradiction
+    --   trivial
+    -- }
+    -- exact (funext test1)
+  }
 
   def rec {β}
           {motive : QpfTree β → Sort}
@@ -198,35 +389,57 @@ namespace QpfTree
   fun given_hyp t => by {
     let t' := Fix.dest t;
     rcases t' with ⟨a, f⟩;
+
+    -- type signature for f post simp mysteriously changed
     cases a ; simp [MvPFunctor.B] at f;
-    -- let children' : (G 0) !![β, Fix Base !![β]] := by {
-    --   let c' := f 0 .fz;
-    --   simp at c';
-    --   let c'' := β;
-    --   suffices t : ((Vec.reverse (Vec.append1 Vec.nil c'')) ::: (Fix Base (Vec.reverse (Vec.append1 Vec.nil c'')))) = (Vec.append1 (Vec.append1 Vec.nil β) (Fix Base (Vec.append1 Vec.nil β)))
+    let required_equality := curried_uncurried_equal β
+
+    -- repair work for mysterious type change
+    let type_equality : (fun i => G i ((Vec.reverse fun i => β) ::: Fix Base (Vec.reverse fun i => β))) 0 = QpfList' (fun (i : Fin2 1) => QpfTree β) := by {
+      sorry
+    }
+
+    let children := f 0 .fz
+    rw [type_equality] at children
+    rw [required_equality] at children
+    let elem : β := f 1 .fz
+
+    let ts5 := given_hyp elem children
+
+    suffices final_equality : (node elem children) = t
+    rw [final_equality] at ts5
+    trivial
+
+    -- extremely weird, this was not an issue before!
+    suffices fix_expansion_equality : Fix.mk ⟨Shape.HeadT.node, f⟩ = t
+
+    let ⟨Shape.HeadT.node, f_left⟩ := Fix.dest (node elem children)
+    -- si
+    -- apply congrArg
+    -- apply congrArg
+
+    -- let t' : ∀ v : TypeVec 2, (G 0) v = QpfList' !![v 0] := by {
+    --   intro v;
+    --   let t1 : G 0 = Comp QpfList' !![@Prj 2 0] := by trivial;
+    --   rw [t1];
+    --   simp [Comp];
+    --   apply congrArg;
+    --   sorry;
     -- };
-    let children' := f 0 .fz;
-    simp at children';
-    let t' : ∀ v : TypeVec 2, (G 0) v = QpfList' !![v 0] := by {
-      intro v;
-      let t1 : G 0 = Comp QpfList' !![@Prj 2 0] := by trivial;
-      rw [t1];
-      simp [Comp];
-      apply congrArg;
-      sorry;
-    };
-    rw [t'] at children';
-    suffices children_converted : QpfList (QpfTree β);
-    let ind_res := given_hyp (f 1 .fz) (children_converted);
-    suffices h : node (f 1 .fz) (children_converted) = t;
-    rw [h] at ind_res;
-    trivial;
-    sorry;
-    sorry;
+    -- rw [t'] at children';
+    -- suffices children_converted : QpfList (QpfTree β);
+    -- let ind_res := given_hyp (f 1 .fz) (children_converted);
+    -- suffices h : node (f 1 .fz) (children_converted) = t;
+    -- rw [h] at ind_res;
+    -- trivial;
+    -- sorry;
+    -- sorry;
   }
 
 end QpfTree
 
 #check funext
+
+
 
 export QpfTree (QpfTree QpfTree')
